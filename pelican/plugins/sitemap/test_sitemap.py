b75ecdae4168ee11b6306b4bec5de96db0d2ec3f
Kernc <kerncece@gmail.com>
2023-07-14 17:14:58 +0200

    Add basic sitemap unit tests for 'txt' and 'xml'

--- pelican/plugins/sitemap/test_sitemap.py
+++ pelican/plugins/sitemap/test_sitemap.py
@@ -1,11 +1,69 @@
 from pathlib import Path
+from shutil import rmtree
+from tempfile import mkdtemp
+import unittest
 
-from . import SitemapGenerator
+from pelican import Pelican
+from pelican.settings import read_settings
+
+from . import sitemap
 
 BASE_DIR = Path(".").resolve()
 TEST_DATA = BASE_DIR / "test_data"
 
 
-def test_sitemap_generator(tmp_path):
-    """Test the Sitemap generator."""
-    SitemapGenerator()
+class TestSitemap(unittest.TestCase):
+    def setUp(self):
+        self.output_path = mkdtemp(prefix="pelican-plugins-sitemap-tests-")
+
+    def tearDown(self):
+        rmtree(self.output_path)
+
+    def _run_pelican(self, sitemap_format):
+        settings = read_settings(
+            override={
+                "CACHE_CONTENT": False,
+                "SITEURL": "http://localhost",
+                "CONTENT": TEST_DATA,
+                "OUTPUT_PATH": self.output_path,
+
+                "PLUGINS": [sitemap],
+                "SITEMAP": {
+                    "format": sitemap_format,
+                },
+            }
+        )
+        pelican = Pelican(settings=settings)
+        pelican.run()
+
+    def test_txt(self):
+        self._run_pelican(sitemap_format="txt")
+        with open(Path(self.output_path) / "sitemap.txt") as fd:
+            contents = fd.read()
+        expected = """\
+http://localhost/
+http://localhost/archives.html
+http://localhost/authors.html
+http://localhost/categories.html
+http://localhost/category/test.html
+http://localhost/tag/bar.html
+http://localhost/tag/foo.html
+http://localhost/tag/foobar.html
+http://localhost/tags.html
+http://localhost/test-post.html
+"""
+        self.assertEqual(expected, contents)
+
+    def test_xml(self):
+        self._run_pelican(sitemap_format="xml")
+        with open(Path(self.output_path) / "sitemap.xml") as fd:
+            contents = fd.read()
+        needle = """\
+<url>
+<loc>http://localhost/test-post.html</loc>
+<lastmod>2023-07-12T13:00:00+00:00</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.5</priority>
+</url>
+"""
+        self.assertIn(needle, contents)
